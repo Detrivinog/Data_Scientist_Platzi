b56da4ce3d09ec8abb0cc9ed087baeb967e57398
Detrivinog <detrivinog@gmail.com>
2022-02-12 21:48:11 -0500

    Curso python intermedio

--- 3. Python Intermedio/high_order_functions.py
+++ 3. Python Intermedio/high_order_functions.py
@@ -7,7 +7,14 @@ def run():
     my_list = range(1, 11)
     odd = list(filter(lambda x: x%2 != 0, my_list))
 
+    squares = list(map(lambda x: x**2, my_list))
+
+    sum_all = reduce(lambda a, b: a + b, my_list)
+
     print(odd)
+    print(squares)
+    print(sum_all)
+
 
 if __name__ == '__main__':
     run()
